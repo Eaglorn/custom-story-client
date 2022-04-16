85e900fc9dd8eb32f35204d3ae5602f7c19d71f0
eaglorn <eaglorn@yandex.ru>
2022-04-16 10:37:48 +1000

    Обновление

---
+++ src/boot/socket.js
@@ -0,0 +1,12 @@
+import { boot } from 'quasar/wrappers';
+import { io } from 'socket.io-client';
+
+const socket = io('http://127.0.0.1:8080');
+
+// "async" is optional;
+// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+export default boot(async ({ app }) => {
+  app.config.globalProperties.$socket = socket;
+});
+
+export { socket };
