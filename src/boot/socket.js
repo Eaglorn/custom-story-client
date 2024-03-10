1bcc8f4496a5a300c60d455de06ce4b78c833a6a
Eaglorn <lexus1.45@mail.ru>
2024-03-10 21:13:54 +1000

    update

--- src/boot/socket.js
+++ src/boot/socket.js
@@ -1,7 +1,7 @@
-import { boot } from 'quasar/wrappers';
-import { io } from 'socket.io-client';
+import { boot } from "quasar/wrappers";
+import { io } from "socket.io-client";
 
-const socket = io('http://127.0.0.1:8080');
+const socket = io("http://127.0.0.1:3000");
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
